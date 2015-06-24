d8bca58f02ce7c6b2f7d1b7304eb424a3515bfdb
redo2011dht <redo2011dht@gmail.com>
2015-06-24 17:11:10 +0700

    using gem activeadmin

---
+++ app/admin/customer.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Customer do
+permit_params :full_name, :phone_number, :email_address, :notes, :province_id
+
+end
---
+++ app/models/customer.rb
@@ -0,0 +1,4 @@
+class Customer < ActiveRecord::Base
+	belongs_to :province
+	validates :full_name,presence: true
+end
